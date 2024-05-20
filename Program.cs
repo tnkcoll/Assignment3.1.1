5036495d70de42fcd864f2c11d6e9ecb704c3f28
Ted Collins <theodore.collins2023@outlook.com>
2024-05-20 13:43:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment3._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[49];
+            
+            numbers = ReturvEvenNumbers(numbers);
+            PrintEvenNumbers(numbers);
+        }
+
+        public static int[] ReturvEvenNumbers(int[] nums)
+        {
+            int j = 0;
+            for (int i = 2; j < nums.Length;)
+            {
+                
+                nums[j] = i;
+                i = i + 2;
+                j++;
+            }
+            return nums;
+        }
+
+        public static void PrintEvenNumbers(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
